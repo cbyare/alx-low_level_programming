6978b640faf5fb8ef4fafe11685c07be6bc74d44
Abdirahman <cbyare444@gmail.com>
2022-12-09 10:13:02 -0800

    6-print-numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers < 10; numbers++)
+	{
+		putchar(numbers);
+	}
+	putchar('\n');
+	return (0);
+}
