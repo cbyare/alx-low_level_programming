f4f04cd3ab73e324a16c2df646789f925f165f50
Abdirahman <cbyare444@gmail.com>
2022-12-09 11:26:07 -0800

    100-print-combi

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numbers;
+	int c;
+
+	for (numbers = 48; numbers < 56; numbers++)
+	{
+		for (c = 49; c <= 57; c++)
+		{
+			if (c > numbers)
+			{
+				putchar(numbers);
+				putchar(c);
+				if (numbers != 56 || c != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
