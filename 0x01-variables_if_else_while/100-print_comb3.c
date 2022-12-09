ab5a38fa97e335141093136146e4b72b042c2baa
Abdirahman <cbyare444@gmail.com>
2022-12-09 11:48:06 -0800

    101-print-com

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,7 @@ int main(void)
 	int numbers;
 	int c;
 
-	for (numbers = 48; numbers < 56; numbers++)
+	for (numbers = 48; numbers <= 56; numbers++)
 	{
 		for (c = 49; c <= 57; c++)
 		{
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numbers, c, d;
+
+	for (numbers = 48; numbers < 58; numbers++)
+	{
+		for (c = 48; c <= 58; c++)
+		{
+			for (d = 50; d <= 58; d++)
+			{
+				if (d > c && c > numbers)
+				{
+					putchar(numbers);
+					putchar(c);
+					putchar(d);
+					if (numbers != 55 || c != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
