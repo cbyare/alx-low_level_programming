09f557d463873ea91cbfc1d2ab9d8cf4928ff2d9
Abdirahman <cbyare444@gmail.com>
2022-12-13 08:40:35 -0800

    11-co

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59
+ * times_table - prints the 9 times table, starting with 0
  */
 void times_table(void)
 {
