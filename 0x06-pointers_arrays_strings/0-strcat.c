98ba2f87af60ae9df87362aff7b95e41bf465ae5
Abdirahman <cbyare444@gmail.com>
2022-12-22 05:34:53 -0800

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,7 +5,6 @@
  * @src: pointer to source input
  * Return: pointer to resulting string @dest
  */
-
 char *_strcat(char *dest, char *src)
 {
 	int c, c2;
@@ -19,6 +18,5 @@ char *_strcat(char *dest, char *src)
 	for (c2 = 0; src[c2] ; c2++)
 		/*append src[c2] to dest[c] while overwritting the null byte in dest*/
 		dest[c++] = src[c2];
-	
 	return (dest);
 }
