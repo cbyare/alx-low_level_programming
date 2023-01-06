14182abb03d79e8a27510211594a132109bab4df
Abdirahman <cbyare444@gmail.com>
2023-01-06 08:47:47 -0800

    all betty syntext checked

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,21 +1,36 @@
+#include "main.h"
 
 /**
- * _strcpy - copies a string from one pointer to another
- * including the terminating null byte (\0)
- * @src: source of string parameter input
- * @dest: destination of string
- * Return: pointer to dest input parameter
+ * _strcpy - copies the string pointed to src, including \0
+ * @src: pointer
+ * @dest: pointer
+ * Return: the pointer to dest
  */
 
-char *_strcpy(char *dest, char *src)
-{
-	int a, b = 0;
 
-	for (a = 0; src[a] != '\0'; ++a)
+
+char *_strcpy(char *dest, char *src)
+
+{
+
+	int i, length;
+
+
+
+	for (length = 0; src[length] != '\0'; length++)
+
 	{
-		dest[b] = src[a];
-		++b;
+
 	}
-	dest[b] = '\0';
+
+	for (i = 0; i <= length; i++)
+
+	{
+
+		dest[i] = src[i];
+
+	}
+
 	return (dest);
+
 }
